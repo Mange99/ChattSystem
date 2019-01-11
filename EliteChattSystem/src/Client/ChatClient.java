3680680a327f51d6667f07dd3d3e764ed22f6b42
vlbins <albinlandgreen@gmail.com>
2019-01-11 15:56:36 +0100

    fix

--- EliteChattSystem/src/Client/ChatClient.java
+++ EliteChattSystem/src/Client/ChatClient.java
@@ -13,7 +13,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-test222
 public class ChatClient {
 
     BufferedReader in;
