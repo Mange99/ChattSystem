5a747f6e89665d9caed04c3af0fa22a486f9a243
Magnus <Magnus.bryntheson@hotmail.com>
2019-01-12 22:43:11 +0100

    albin test

--- EliteChattSystem/src/GUI/GUI.java
+++ EliteChattSystem/src/GUI/GUI.java
@@ -1,24 +1,22 @@
 package GUI;
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+	
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.ScrollPaneConstants;
 
 import Client.ChatClient;
 
 public class GUI {
     private JFrame frame = new JFrame("Chatter");
+    
+    Font f = new Font("Verdana",Font.BOLD,30);
+    
     private JTextField textField;
     private JTextArea messageArea;
     private JLabel labels[];
